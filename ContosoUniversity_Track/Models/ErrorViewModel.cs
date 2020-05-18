de1ae74405877735805d311ecc790540892ceb13
Charles eteye <charlesdev48@gmail.com>
2020-05-18 18:53:12 +0300

    Add project files.

---
+++ ContosoUniversity_Track/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContosoUniversity_Track.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
